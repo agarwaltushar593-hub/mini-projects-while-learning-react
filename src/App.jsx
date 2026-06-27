08f30848944dbedc503fa505c8b792244c301590
Tushar Agarwal <agarwaltushar593@gmail.com>
2026-06-27 08:34:03 +0530

    To-Do list

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+
+function App() {
+  return <center class='topic'>
+           <h1>To-Do List</h1>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-4">
+          <input type="text" placeholder="enter To-do list" />
+        </div>
+        <div class="col-2">
+          <input type="date" />
+        </div>
+        <div class="col-2">
+          <div class="btn">
+            <button type="button" class="btn btn-success">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class ="container text-center">
+    <div class ="row">
+    <div class ="col-4">
+    do work
+    </div>
+    <div class ="col-2">
+    dd/mm/yyyy
+     </div>
+      <div class ="col-2">
+        <div class ="btn">
+          <button type ="button" class ="btn btn-danger">delete </button>
+        </div>
+      </div>
+     </div>
+    </div>
+    
+    <div class ="container text-center">
+    <div class ="row">
+    <div class ="col-4">
+    do work
+    </div>
+    <div class ="col-2">
+    dd/mm/yyyy
+     </div>
+      <div class ="col-2">
+        <div class ="btn">
+          <button type ="button" class ="btn btn-danger">delete </button>
+        </div>
+      </div>
+     </div>
+    </div>
+  </center>
+}
+
+export default App;
